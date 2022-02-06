5b4fdfc0b99f7d9dd470a994fdc4692c423a32b3
Kelvin <kelvin@gokwik.co>
2022-02-06 22:36:00 +0530

    Added Pod file
    Added binary files for Pod.

---
+++ Pod/GokwikUpi.xcframework/ios-x86_64-simulator/GokwikUpi.framework/Headers/GokwikUpi.h
@@ -0,0 +1,18 @@
+//
+//  GokwikUpi.h
+//  GokwikUpi
+//
+//  Created by Himanshu Sonker on 20/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GokwikUpi.
+FOUNDATION_EXPORT double GokwikUpiVersionNumber;
+
+//! Project version string for GokwikUpi.
+FOUNDATION_EXPORT const unsigned char GokwikUpiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GokwikUpi/PublicHeader.h>
+
+
